4121b76fc1cbdbe835cbf2183012f6b44671e16d
HanfiyZQ <zamaksyaryhanfiy@gmail.com>
2022-04-27 07:24:59 +0700

    Hooks and Fetch

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,21 +1,39 @@
+import { useEffect, useState } from "react";
 import styles from "./Hero.module.css";
 
 function Hero() {
+    // membuat state movie
+    const [movie,setMovie] = useState("");
+
+    async function fetchMovie() {
+        // melakukan side effects: fetch data movie (API);
+        const response = await fetch("https://www.omdbapi.com/?apikey=fcf50ae6&i=tt2975590");
+
+        const data = await response.json();
+
+        // set movie dengan data movie hasil fetch
+        setMovie(data);
+    }
+
+    useEffect(fetchMovie,[]);
+    console.log(movie);
+
+
     return (
         <div className={styles.container}>
             <section className={styles.hero}>
                 <div className={styles.hero__left}>
-                    <h2 className={styles.hero__title}>Jujutsu Kaisen 0</h2>
-                    <h3 className={styles.hero__genre}>Anime, Action, Mystery</h3>
+                    <h2 className={styles.hero__title}>{movie.Title}</h2>
+                    <h3 className={styles.hero__genre}>{movie.Genre}</h3>
                     <p className={styles.hero__description}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quasi sequi alias quaerat cum, iusto rerum velit omnis ad maiores.
+                        {movie.Plot}
                     </p>
                     <button className={styles.hero__button}>Watch</button>
                 </div>
                 <div className={styles.hero__right}>
                     <img
                         className={styles.hero__image}
-                        src="https://picsum.photos/600/400" alt="" />
+                        src={movie.Poster} alt={movie.Title} />
                 </div>
             </section>
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import AddMovieForm from "../components/AddMovieForm/AddMovieForm";
+import Counter from "../components/Counter";
 import Hero from "../components/Hero/Hero";
 import Movies from "../components/Movies/Movies";
 import data from "../utils/constants/data";
@@ -13,6 +14,10 @@ function Home() {
             <AddMovieForm movies={movies} setMovies={setMovies} />
         </>
     );
+
+    // return(
+    //     <Counter />
+    // );
 }
 
 export default Home;
